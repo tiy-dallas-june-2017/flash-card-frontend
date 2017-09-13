b42b8a0b648db4d9f3ae223a5d9ab0c36d844f04
Jeff Irwin <jeff.irwin@live.com>
2017-09-13 14:30:08 -0500

    removed unused functions and state

--- src/components/quizOptions.js
+++ src/components/quizOptions.js
@@ -2,22 +2,13 @@ import React from 'react';
 
 export default class QuizOptions extends React.Component {
 
-  state = {
-    showForm: true
-  }
-
-  closeQuizOptionsDiv = () => {
-    this.setState({
-      showForm: false
-    })
-  }
 
   render() {
     console.log('props', this.props);
     return (
       <div className={this.props.showForm ? 'outer' : 'hidden'}>
         <div className='inner'>
-          <div className='close' onClick={this.props.toggleForm}>x</div>
+          <div className='close' onClick={this.props.toggleForm}>&times;</div>
           <h1>Start new quiz</h1>
           <div className='button option'>random</div>
           <div className='button option'>new cards</div>
