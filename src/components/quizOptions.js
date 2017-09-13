7ce7c4f5f07284956769d5d549acd5c7e8d68dd3
Jeff Irwin <jeff.irwin@live.com>
2017-09-13 10:23:42 -0500

    display quiz options for sets with more than 10 cards

---
+++ src/components/quizOptions.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default class QuizOptions extends React.Component {
+  render() {
+
+    console.log('props', this.props);
+    return (
+      <div className={this.props.showForm ? 'outer' : 'hidden'}>
+        <div className='inner'>
+          <h1>Start new quiz</h1>
+          <div className='button option'>random</div>
+          <div className='button option'>new cards</div>
+          <div className='button option'>cards I don't know</div>
+        </div>
+      </div>
+    )
+  }
+}
